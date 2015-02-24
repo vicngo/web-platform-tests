02f96867c4c121b69ae41ababff7375219c6b798
James Graham <james@hoppipolla.co.uk>
2015-02-24 17:51:32 +0000

    fixup! fixup! Start at making a testsuite that can also cover low-level features

--- webdriver/tests/test_get.py
+++ webdriver/tests/test_get.py
@@ -7,7 +7,7 @@ class Test(TestBase):
         t.assert_equals(resp.data, {})
 
     def test_get_data(self):
-        resp = self.session.get("data:test/html,<p>Test")
+        resp = self.session.get("data:text/html,<p>Test")
         t.assert_resp(resp, status=200)
         t.assert_equals(resp.data, {})
 
@@ -24,3 +24,9 @@ class Test(TestBase):
     def test_get_no_url(self):
         t.assert_error(self.session.get(None, raw_body={}), "invalid argument")
 
+    def test_get_relative_url(self):
+        resp = self.session.get(self.session.url("/"))
+        t.assert_resp(resp, status=200)
+        t.assert_equals(resp.data, {})
+        resp = self.session.get("/common/blank.html")
+        t.assert_error(resp, "invalid argument")
